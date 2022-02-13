823e187d575b0dfc90e0c42afee3621b53f7d052
aamir0143 <mdaamirreza00@gmail.com>
2022-02-13 20:35:28 +0530

    [Aamir20] Add : Welcome To Mood Analyser Programs

---
+++ MoodAnalyserProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MoodAnalyserProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Mood Analyser Problems");
+            Console.ReadLine();
+        }
+    }
+}
