62e85c51cb4e7fba4f5e0867ae90daea5b242de0
aamir0143 <mdaamirreza00@gmail.com>
2022-02-15 00:10:14 +0530

    [Aamir20] Add : Given A Message Happy or Sad Mood

---
+++ MoodAnalyserProblems/MoodAnalyser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MoodAnalyserProblems
+{
+    /// <summary>
+    /// Creating Mood Analyser Class.
+    /// </summary>
+    public class MoodAnalyser
+    {
+        //Declaring Variables.
+        public string message;
+        //Parameterized Constructors.
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        /// <summary>
+        /// Creating Method for Happy Or Sad Mood.
+        /// </summary>
+        /// <returns></returns>
+        public string AnalyseMood()
+        {
+            if (this.message.ToLower().Contains("sad"))
+            {
+                return "sad";
+            }
+            else
+            {
+                return "happy";
+            }
+        }
+    }
+}
--- MoodAnalyserProblems/Program.cs
+++ MoodAnalyserProblems/Program.cs
@@ -7,6 +7,10 @@ namespace MoodAnalyserProblems
         static void Main(string[] args)
         {
             Console.WriteLine("==========Welcome To Mood Analyser Problems");
+            //Calling the mood analyser object(UC1)
+            MoodAnalyser mood = new MoodAnalyser("Today Is A Happy Day For Me");
+            string resMood = mood.AnalyseMood();
+            Console.WriteLine("The mood is {0}", resMood);
             Console.ReadLine();
         }
     }
